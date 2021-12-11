35db08cc72c28af84c33d527677da7d4a6cec760
Okan Karaduman <okan_karadmn@hotmail.com>
2021-12-11 23:20:59 +0300

    post and get requests correctly working

--- restapiexample/models/employees.go
+++ restapiexample/models/employees.go
@@ -12,8 +12,8 @@ var list_employee []Employees
 
 type Employees struct {
 	Id         int    `json:id`
-	Firstname  string `json:first_name`
-	LastName   string `json:last_name`
+	FirstName  string `json:firstname`
+	LastName   string `json:lastname`
 	Department string `json:department`
 	Age        int    `json:age`
 	Salary     int    `json:salary`
@@ -26,15 +26,16 @@ func Employee(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		resp := utils.Response{Resp: w}
-		resp.Text(http.StatusOK, "Employee Info:\n", "text/plain")
+		body, err := json.Marshal(list_employee)
+		utils.CheckError(err)
+		resp.Text(http.StatusOK, string(body), "text/plain")
 	case http.MethodPost:
 		err := json.NewDecoder(r.Body).Decode(&e)
 		utils.CheckError(err)
-		fmt.Println(e.Firstname)
 		list_employee = append(list_employee, e)
 		fmt.Println(list_employee)
 		resp := utils.Response{Resp: w}
-		resp.Text(http.StatusOK, "Operation Successful! \n"+e.Firstname+" is added to our Company!", "text/plain")
+		resp.Text(http.StatusOK, "Operation Successful! \n"+e.FirstName+" "+e.LastName+" is added to our Company!", "text/plain")
 	default:
 		resp := utils.Response{Resp: w}
 		resp.Text(http.StatusMethodNotAllowed, "Status not allowed", "text/plain")
---
+++ restapiexample/utils/router.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"net/http"
+	"regexp"
+)
+
+type route struct {
+	pattern *regexp.Regexp
+	handler http.Handler
+}
+
+type RegexpHandler struct {
+	routes []*route
+}
+
+func (h *RegexpHandler) Handler(pattern *regexp.Regexp, handler http.Handler) {
+	h.routes = append(h.routes, &route{pattern, handler})
+}
+
+func (h *RegexpHandler) HandleFunc(pattern *regexp.Regexp, handler func(http.ResponseWriter, *http.Request)) {
+	h.routes = append(h.routes, &route{pattern, http.HandlerFunc(handler)})
+}
